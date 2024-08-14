408c776c01f6620feb7507b995d786dcb883f837
GrieVeR-13 <GrieVeR-C@list.ru>
2024-08-14 13:24:36 +0300

    Upd: drive module

--- drive/share/data/build.gradle.kts
+++ drive/share/data/build.gradle.kts
@@ -1,36 +1,29 @@
-/*
- * Copyright (c) 2021-2023 Proton AG.
- * This file is part of Proton Core.
- *
- * Proton Core is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Proton Core is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Proton Core.  If not, see <https://www.gnu.org/licenses/>.
- */
+import Presentation_android_common_gradle.FlavorsConfiguratorPlugin.configureFlavors
 
 plugins {
-    id("com.android.library")
+    id(libs.plugins.comAndroidLibrary)
+    id(libs.plugins.kotlinAndroid)
+    kotlin(libs.plugins.pluginSerialization)
 }
+
+configureFlavors()
 
 android {
+    compileSdk = Tools.Android.compileSdkVersion
     namespace = "me.proton.core.drive.share.data"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    defaultConfig {
+        minSdk = Tools.Android.minSdkVersion
+        consumerProguardFiles("proguard-rules.pro")
+    }
 }
 
-driveModule(
-    hilt = true,
-    serialization = true,
-    room = true,
-) {
-    api(project(":drive:base:data"))
-    api(project(":drive:share:domain"))
-    implementation(libs.retrofit)
-    implementation(libs.androidx.dataStore.preferences)
+dependencies {
+    implementation(libs.androidxCore.coreKtx)
+    implementation(libs.orgJetbrainsKotlinx.kotlinxSerializationJson)
 }
